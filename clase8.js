2b72f232e765ab5ada6ce6fcf3ce7359eae4f60e
Victor Varela <victor398@gmail.com>
2022-10-25 11:06:49 -0300

    clase 8 DOM

---
+++ clase8.js
@@ -0,0 +1,15 @@
+//DOM
+
+let fecha = document.getElementsByClassName("fecha")
+console.log(fecha)
+fecha.innerText = "hoy es hoy"
+console.log(fecha.innerText)
+
+let titulos = document.getElementById("titulo")
+
+console.log(titulos)
+titulos.innerText = "TODO CAMBIA"
+console.log(titulos.innerText)
+
+let lista= document.getElementById("lista")
+console.log(lista.innerHTML)
